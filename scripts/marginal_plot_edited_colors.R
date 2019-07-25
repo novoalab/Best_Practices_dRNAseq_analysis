883177479911778a2c765f7a7f0685146ace8102
José Miguel Ramírez <34416160+JMiguelRamirez@users.noreply.github.com>
2019-07-25 17:49:15 +0200

    Creating scripts folder

---
+++ scripts/marginal_plot_edited_colors.R
@@ -0,0 +1,117 @@
+marginal_plot = function(x, y, group = NULL, data = NULL, lm_formula = y ~ x, bw = "nrd0", alpha = 1, plot_legend = T, ...){
+  require(scales)
+  ###############
+  # Plots a scatterplot with marginal probability density functions for x and y. 
+  # Data may be grouped or ungrouped. 
+  # For each group, a linear fit is plotted. The model can be modified using the 'lm_formula' argument. Setting 'lm_formula' to NULL prevents plotting model fits.
+  # The 'bw' argument specifies the bandwidth rule used for estimating probability density functions. See ?density for more information.
+  # For large datasets, opacity may be decreased by setting alpha to a value between 0 and 1.
+  # Additional graphical parameters are passed to the main plot, so you can customize axis labels, titles etc.
+  ###############
+  moreargs = eval(substitute(list(...)))
+  
+  # prepare consistent df
+  if(missing(group)){
+    if(missing(data)){
+      if(length(x) != length(y)){stop("Length of arguments not equal")}
+      data = data.frame(x = as.numeric(x), y = as.numeric(y))
+    } else {
+      data = data.frame(x = as.numeric(data[,deparse(substitute(x))]), 
+                        y = as.numeric(data[,deparse(substitute(y))]))
+    }
+    if(sum(!complete.cases(data)) > 0){
+      warning(sprintf("Removed %i rows with missing data", sum(!complete.cases(data))))
+      data = data[complete.cases(data),]
+    }
+    group_colors = "black"
+  } else {
+    if(missing(data)){
+      if(length(x) != length(y) | length(x) != length(group)){stop("Length of arguments not equal")}
+      data = data.frame(x = as.numeric(x), y = as.numeric(y), group = as.factor(group))
+    } else {
+      data = data.frame(x = as.numeric(data[,deparse(substitute(x))]), 
+                        y = as.numeric(data[,deparse(substitute(y))]),
+                        group = as.factor(data[,deparse(substitute(group))]))
+    }
+    if(sum(!complete.cases(data)) > 0){
+      warning(sprintf("Removed %i rows with missing data", sum(!complete.cases(data))))
+      data = data[complete.cases(data),]
+    }
+    data = subset(data, group %in% names(which(table(data$group) > 5)))
+    data$group = droplevels(data$group)
+    group_colors = rainbow(length(unique(data$group)) + 1)[-2] #When there are only 2 colours, we get a very light blue, in this way, we get a darker blue
+    #group_colors = c(rgb(1,0.5,0.5,0.5),rgb(0,0.5,1,0.5),rainbow(length(unique(data$group))))
+  } 
+  
+  # log-transform data (this is need for correct plotting of density functions)
+  if(!is.null(moreargs$log)){
+    if(!moreargs$log %in% c("y", "x", "yx", "xy")){
+      warning("Ignoring invalid 'log' argument. Use 'y', 'x', 'yx' or 'xy.")
+    } else {
+      data = data[apply(data[unlist(strsplit(moreargs$log, ""))], 1, function(x) !any(x <= 0)), ]
+      data[,unlist(strsplit(moreargs$log, ""))] = log10(data[,unlist(strsplit(moreargs$log, ""))])
+    }
+    moreargs$log = NULL # remove to prevent double logarithm when plotting
+  }
+  
+  # Catch unwanted user inputs
+  if(!is.null(moreargs$col)){moreargs$col = NULL}
+  if(!is.null(moreargs$type)){moreargs$type = "p"}
+  
+  # get some default plotting arguments
+  if(is.null(moreargs$xlim)){moreargs$xlim = range(data$x)} 
+  if(is.null(moreargs$ylim)){moreargs$ylim = range(data$y)}
+  if(is.null(moreargs$xlab)){moreargs$xlab = deparse(substitute(x))}
+  if(is.null(moreargs$ylab)){moreargs$ylab = deparse(substitute(y))}
+  if(is.null(moreargs$las)){moreargs$las = 1} 
+  
+  # plotting
+  tryCatch(expr = {
+    ifelse(!is.null(data$group), data_split <- split(data, data$group), data_split <- list(data))
+    orig_par = par(no.readonly = T)
+    par(mar = c(0.25,5,1,0))
+    layout(matrix(1:4, nrow = 2, byrow = T), widths = c(10,3), heights = c(3,10))
+    
+    # upper density plot
+    plot(NULL, type = "n", xlim = moreargs$xlim, ylab = "density",
+         ylim = c(0, max(sapply(data_split, function(group_set) max(density(group_set$x, bw = bw)$y)))), main = NA, axes = F)
+    axis(2, las = 1)
+    mapply(function(group_set, group_color){lines(density(group_set$x, bw = bw), col = group_color, lwd = 2)}, data_split, group_colors)
+    
+    # legend
+    par(mar = c(0.25,0.25,0,0))
+    plot.new()
+    if(!missing(group) & plot_legend){
+      legend("center", levels(data$group), fill = group_colors, border = group_colors, bty = "n", title = deparse(substitute(group)), title.adj = 0.25)
+    }
+    
+    # main plot
+    par(mar = c(4,5,0,0))
+    if(missing(group)){
+      do.call(plot, c(list(x = quote(data$x), y = quote(data$y), col = quote(scales::alpha("black", alpha))), moreargs))
+    } else {
+      do.call(plot, c(list(x = quote(data$x), y = quote(data$y), col = quote(scales::alpha(group_colors[data$group], alpha))), moreargs))
+    }
+    axis(3, labels = F, tck = 0.01)
+    axis(4, labels = F, tck = 0.01)
+    box()
+    
+    if(!is.null(lm_formula)){
+      mapply(function(group_set, group_color){
+        lm_tmp = lm(lm_formula, data = group_set)
+        x_coords = seq(min(group_set$x), max(group_set$x), length.out = 100)
+        y_coords = predict(lm_tmp, newdata = data.frame(x = x_coords))
+        lines(x = x_coords, y = y_coords, col = group_color, lwd = 2.5)
+      }, data_split, rgb(t(ceiling(col2rgb(group_colors)*0.8)), maxColorValue = 255))
+    }
+    
+    # right density plot
+    par(mar = c(4,0.25,0,1))
+    plot(NULL, type = "n", ylim = moreargs$ylim, xlim = c(0, max(sapply(data_split, function(group_set) max(density(group_set$y, bw = bw)$y)))), main = NA, axes = F, xlab = "density")
+    mapply(function(group_set, group_color){lines(x = density(group_set$y, bw = bw)$y, y = density(group_set$y, bw = bw)$x, col = group_color, lwd = 2)}, data_split, group_colors)
+    axis(1)
+  }, finally = {
+    par(orig_par)
+  })
+}
+
---
+++ scripts/per_read.R
@@ -0,0 +1,100 @@
+######################################################################################################################################
+#This script reads n files with a name starting by "OUTPUT_", containing for each base-called read its ID, its length and its mean quality
+#It outputs a boxplot comparing the mean read qualities between approaches and another one comparing the read lengths per base-caller.
+#It also compares how the reads base-called by AL 2.3.4 but not by AL 2.1.7 look regarding qualities and lengths. 
+#And this same comparison for the reads base-called by GU 3.0.3 and not by GU 2.3.1
+######################################################################################################################################
+
+
+#Loading libraries
+
+
+required.packages <- c("ggplot2","ggExtra","optparse")
+new.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+suppressMessages(library(ggplot2))
+library(ggExtra)
+library(optparse)
+source("scripts/marginal_plot_edited_colors.R")
+
+#Parsing
+
+parser <- OptionParser()
+parser <- add_option(parser, opt_str=c("-i", "--input"), type="character",
+                     dest='input',
+                     help="Input Directory")
+parser <- add_option(parser, opt_str=c("-t", "--title"), type="character",
+                     default = "",
+                     dest='title',
+                     help="Title")
+parser <- add_option(parser, opt_str=c("-n", "--names"), type="character",
+                     default = "1 2",
+                     dest='names',
+                     help="Names of the datasets")
+options=parse_args(parser)
+dir=options$input 
+title=options$title
+names=options$names
+
+print(names)
+#Loading data:
+n <- strsplit(names, ",")
+files <- list.files(path=dir, pattern = "OUTPUT_.*")
+
+l <- list()
+table <- c()
+for (i in 1:length(files)){
+  assign(files[i],read.delim(paste(dir, files[i], sep="/")))
+  l[[i]] <- get(files[i])
+  l[[i]]$dataset <- n[[1]][i]
+  table <- rbind(table, l[[i]])
+}
+
+
+#Plotting read lengths
+
+title=paste("Read Length", title)
+
+g <- ggplot(table, aes(dataset, log(read_length))) + 
+  geom_boxplot(outlier.size = 0.7, colour=c(colorRampPalette(c("red", "blue"))(length(files))), 
+               fill=c(colorRampPalette(c("red", "blue"))(length(files))), alpha=0.4) + 
+  ylab("read length (log)") + ggtitle(title) + xlab("") + theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 9), 
+        axis.text=element_text(size=4), 
+        axis.title=element_text(size=6))
+ggsave("read_length.png", g, device = "png", path = dir, width = 1.8, height = 2.5)
+
+#Plotting qualities
+
+title=paste("Mean Quality", title)
+g <- ggplot(table, aes(dataset, log(mean_quality))) + 
+  geom_boxplot(outlier.size = 0.7, colour=c(colorRampPalette(c("red", "blue"))(length(files))), 
+               fill=c(colorRampPalette(c("red", "blue"))(length(files))), alpha=0.4) + 
+  ylab("mean quality (log)") + ggtitle(title) + xlab("") + theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 9), 
+        axis.text=element_text(size=4), 
+        axis.title=element_text(size=6))
+ggsave("quality.png", g, device = "png", path = dir, width = 1.8, height = 2.5)
+
+# Plotting comparisons between versions
+
+#The following plots will compare the reads base-called by both versions of each base-caller against the reads only 
+#base-called by the new version.
+
+# Albacore
+
+#we check whether a read in the new version of Albacore was also base-called by the old version or it is new.
+for (i in 1:length(files)){
+  for (j in 1:length(files)){
+    if (i!=j){
+      l[[i]]$Condition <- l[[i]]$read_id %in% l[[j]]$read_id
+      if(nrow(l[[i]][l[[i]]$Condition==FALSE,])!=0){
+        l[[i]][l[[i]]$Condition==FALSE,]$Condition <- "NEW"}
+      l[[i]][l[[i]]$Condition==TRUE,]$Condition <- "COMMON"
+      png(paste(dir, "/",n[[1]][i], "_against_", n[[1]][j], ".png", sep=""))
+      marginal_plot(x = read_length, y = mean_quality, group = Condition, data = l[[i]], alpha = 0.5, bw = "nrd", lm_formula = NULL, 
+                    xlab = "Read Length (log)", ylab = "Mean Quality (log)", pch = 15, cex = 0.5, log="xy")
+      dev.off()
+}}}
+
