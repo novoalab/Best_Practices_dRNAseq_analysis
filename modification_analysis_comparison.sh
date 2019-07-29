228cb50a153317f8157508d148b6a2ce14f002f6
José Miguel Ramírez <34416160+JMiguelRamirez@users.noreply.github.com>
2019-07-29 14:27:25 +0200

    Add files via upload

--- modification_analysis_comparison.sh
+++ modification_analysis_comparison.sh
@@ -1,12 +1,18 @@
-base=$1
-unm=$2
-mod=$3
-OUTPUT=$4
-model=$5
+#!/bin/bash
+while getopts b:u:m:o:e:n: option
+do
+case "${option}"
+in
+b) base=${OPTARG};;       #modified base
+u) unm=${OPTARG};;        #path to file with unmodified data .csv
+m) mod=${OPTARG};;		  #path to file with modified data .csv
+o) OUTPUT=${OPTARG};;     #output dir
+e) model=${OPTARG};;      #boolean for building a svm model 
+n) name=${OPTARG};;		  #string with the dataset names separated with a comma
+esac
+done
 
-echo $model
-
-#filtering needed for building epinano model
+#filtering needed for curlcakes
 for i in $unm $mod; do 
 head -1 $i > ${i%.*}.filtered.csv
 awk -F"," "\$1 ~ /[^/$base/][^/$base/][/$base/][^/$base/][^/$base/]/" $i >> ${i%.*}.filtered.csv
@@ -16,7 +22,7 @@ if [[ $model == true ]]; then
 
 echo Building epinano model...
 
-cut -d "," -f 5-14,20-24 ${unm} > test
+cut -d "," -f 5-14,20-24 ${unm%.*}.filtered.csv > test
 tail -n +2 test > test2
 sed 's/$/,UNM/' test2 > test3
 a=$( wc -l test3 )
@@ -28,7 +34,7 @@ echo q1,q2,q3,q4,q5,mis1,mis2,mis3,mis4,mis5,del1,del2,del3,del4,del5,sample > p
 head -$t test3 >> training 
 tail -$p test3 >> predicting
 
-cut -d "," -f 5-14,20-24 ${mod} > test
+cut -d "," -f 5-14,20-24 ${mod%.*}.filtered.csv > test
 tail -n +2 test > test2
 sed 's/$/,MOD/' test2 > test3
 head -$t test3 >> training 
@@ -49,11 +55,11 @@ echo No model option provided; fi
 
 echo Plotting...
 
-if [ -z "$6" ]; then
+if [ -z $name ]; then
 	echo Using "UNM,MOD" as dataset names
 	Rscript scripts/kmer.R -u ${unm%.*}.filtered.csv -m ${mod%.*}.filtered.csv -o $OUTPUT
 else
-	Rscript scripts/kmer.R -u ${unm%.*}.filtered.csv -m ${mod%.*}.filtered.csv -o $OUTPUT -n $6
+	Rscript scripts/kmer.R -u ${unm%.*}.filtered.csv -m ${mod%.*}.filtered.csv -o $OUTPUT -n $name
 fi
 
 
